64703d3763484ffa9276806c95868e9b358c4e71
vsmi <vika.smirnowa@yandex.ru>
2022-03-10 22:52:48 +0300

    Add tests for auth

---
+++ test/contest.py
@@ -0,0 +1,51 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from urllib.parse import quote  
+import psycopg2
+import pytest
+from psycopg2.extras import RealDictCursor
+import time
+from app.config import settings
+from app.database import get_db, Base
+from app.main import app
+from fastapi.testclient import TestClient
+
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@pytest.fixture()
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+
+@pytest.fixture()
+def test_user(client):
+    user_data = {"email": "usertest@gmail.com", "password": "password123"}
+    res = client.post("/users/", json = user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user["password"] = user_data["password"]
+    return new_user
+    
+
---
+++ test/test_auth.py
@@ -0,0 +1,30 @@
+from .contest import test_user, client, session
+from jose import jwt
+from app.config import settings
+from app import schemas
+import pytest
+
+def test_login_user(test_user, client):
+    res = client.post("/login", data = {"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    id: str = payload.get("user_id") 
+
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
+
+
+@pytest.mark.parametrize("email, password, status_code",
+[('usertest@gmail.com', 'password', 404),
+('user@gmail.com', 'password123', 404),
+('user@gmail.com', 'password', 404)])
+
+def test_incorrect_login(test_user, client, email, password, status_code):
+
+    res = client.post("/login", data = {"username": email, "password": password})
+    assert res.status_code == status_code
+
+
+
+
